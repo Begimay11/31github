a79b841cd21ae34d788512571f7b86b6f896e8fa
Begimay11 <begimaysagynbaeva245@gmail.com>
2023-12-16 19:29:10 +0600

    lesso

---
+++ lesson14/script.js
@@ -0,0 +1,13 @@
+// unshift -> bashyna el koshot
+// shift-> bashyndagy el ochurot
+// pop()=> ayagyndagy el ochurot
+// push()=> ayagyna el koshot.
+
+
+
+// let str = 'Begimayka'
+// let res = str.split('').reverse().map((el)=> el.toUpperCase()).reverse().join('')
+// console.log(res);
+
+
+
