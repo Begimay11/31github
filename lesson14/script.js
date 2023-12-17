3d3f36d9b3e6daf83a84fa7c7e5ae6c4ee697de9
Begimay11 <begimaysagynbaeva245@gmail.com>
2023-12-17 11:41:42 +0600

    els

--- lesson14/script.js
+++ lesson14/script.js
@@ -9,5 +9,29 @@
 // let res = str.split('').reverse().map((el)=> el.toUpperCase()).reverse().join('')
 // console.log(res);
 
+// let sum = [12,78,23,43]
+// console.log(Math.min(...sum));
+
+// console.log(Math.pow(4,4));
+
+// console.log(Math.log1p(3)); ?
 
 
+// function usdcny(usd) {
+//     let res = `${usd * 6.75} Chinese Yuan`;
+//     return res
+//     // return usd.toString().length >= 4 ? sum : res
+// }
+// // console.log(`${usdcny(15)} Chinese Yuan`);
+// console.log(usdcny(190));
+
+// let age = prompt("Сколько Вам лет?");
+// age > 14 ? alert('Kruto') : alert('Jakshyy');
+
+
+
+
+//   let ask = (ques,yes,not)=> {
+//     confirm(ques) ? yes() : not()
+//   }
+//   ask("Вы согласны?",()=> {alert('Вы согласились.')},function() {alert('Вы отменили выполнение.')})
