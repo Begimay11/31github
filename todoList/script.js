33a06ac4b5a317bb3690431029c1e2bb426792e9
Begimay11 <begimaysagynbaeva245@gmail.com>
2023-12-23 17:55:20 +0600

    circle

--- todoList/script.js
+++ todoList/script.js
@@ -1,40 +1,69 @@
-const container = document.querySelector('.container')
-const active = document.querySelector('.active')
-const clear = document.querySelector('.clear')
+// const container = document.querySelector('.container')
+// const active = document.querySelector('.active')
+// const clear = document.querySelector('.clear')
+const charac = document.querySelector('.charac')
 
-let n = 600
+// const character = document.querySelector('.character')
 
-for(let i = 1; i <= n; i++){
-    const square = document.createElement('div')
-    square.setAttribute('class','square')
-    container.append(square)
-    square.addEventListener('mouseover',()=> {
-        square.style.background = randomColor()
-        square.style.boxShadow = `0 0 10px ${randomColor()}`
+// let n = 400
+
+// for(let i = 1; i <= n; i++){
+//     const square = document.createElement('div')
+//     square.setAttribute('class','square')
+//     container.append(square)
+//     square.addEventListener('mouseover',()=> {
+//         square.style.background = randomColor()
+//         square.style.boxShadow = `0 0 10px ${randomColor()}`
+//     })
+//     square.addEventListener('mouseleave',()=> {
+//         square.style.background = ''
+//         square.style.boxShadow = ''
+//     })
+//     active.addEventListener('click',()=> {
+//         square.style.background = randomColor()
+//     })
+//     clear.addEventListener('click',()=> {
+//         square.style.background = ''
+// })
+// }
+
+// const randomColor = ()=> {
+//     let color = '#'
+//     for(let i = 1; i <= 6; i++){
+//      color += Math.floor(Math.random() * 10)
+//     }
+//     return color
+// }
+
+
+let m = 3000
+for(let i = 0; i< m; i++){
+    const character = document.createElement('div')
+    character.setAttribute('class','character')
+    charac.append(character)
+
+    character.addEventListener('mouseover', ()=> {
+        character.style.background = randomDiv()
     })
-    square.addEventListener('mouseleave',()=> {
-        square.style.background = ''
-        square.style.boxShadow = ''
+    character.addEventListener('mouseout', ()=> {
+        // character.style.background = ''
     })
 }
-function activeColor(){
-    active.addEventListener('click',activeColor())
-}
-// active.addEventListener('click',()=> {
-//     square.forEach((el)=> {
-//         el.style.background = randomColor()
-//     })
-// })
-// clear.addEventListener('click',()=> {
-//     container.forEach(el=> {
-//         el.style.background = 'yellow'
-//     })
-// })
-
-const randomColor = ()=> {
-    let color = '#'
-    for(let i = 1; i <= 6; i++){
-        color += Math.floor(Math.random() * 10)
+const randomDiv = ()=> {
+    let randomer = '#'
+    for(let i = 1; i<= 6; i++){
+        randomer += Math.floor(Math.random() * 10)
     }
-    return color
+    return randomer
 }
+
+
+// myName()
+// function myName(){
+//     window.addEventListener('mousemove', (e)=> {
+//         const target = e.target
+//         if(!target) return
+//         charac.style.left = e.pageX + 'px'
+//         charac.style.top = e.pageY + 'px'
+//     })
+// }
