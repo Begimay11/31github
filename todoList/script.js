fb9b56c8ca695da769f3455b228a8cb280b65405
Begimay11 <begimaysagynbaeva245@gmail.com>
2023-12-25 15:43:08 +0600

    todoList

--- todoList/script.js
+++ todoList/script.js
@@ -1,79 +1,43 @@
-// const container = document.querySelector('.container')
-// const active = document.querySelector('.active')
-// const clear = document.querySelector('.clear')
-const charac = document.querySelector('.charac')
+const textInput = document.querySelector('.text')
+const numberInput = document.querySelector('.number')
+const addBtn = document.querySelector('button')
+const newList = document.querySelector('.newList')
+const addlist = document.querySelector('.addlist')
+const textObe = document.querySelector('.obe')
 
-// const character = document.querySelector('.character')
-
-// let n = 400
-
-// for(let i = 1; i <= n; i++){
-//     const square = document.createElement('div')
-//     square.setAttribute('class','square')
-//     container.append(square)
-//     square.addEventListener('mouseover',()=> {
-//         square.style.background = randomColor()
-//         square.style.boxShadow = `0 0 10px ${randomColor()}`
-//     })
-//     square.addEventListener('mouseleave',()=> {
-//         square.style.background = ''
-//         square.style.boxShadow = ''
-//     })
-//     active.addEventListener('click',()=> {
-//         square.style.background = randomColor()
-//     })
-//     clear.addEventListener('click',()=> {
-//         square.style.background = ''
-// })
-// }
-
-// const randomColor = ()=> {
-//     let color = '#'
-//     for(let i = 1; i <= 6; i++){
-//      color += Math.floor(Math.random() * 10)
-//     }
-//     return color
-// }
-
-
-let m = 3000
-for(let i = 0; i< m; i++){
-    const character = document.createElement('div')
-    character.setAttribute('class','character')
-    charac.append(character)
-
-    character.addEventListener('mouseover', ()=> {
-        character.style.background = randomDiv()
-    })
-    character.addEventListener('mouseout', ()=> {
-        // character.style.background = ''
-    })
-}
-const randomDiv = ()=> {
-    let randomer = '#'
-    for(let i = 1; i<= 6; i++){
-        randomer += Math.floor(Math.random() * 10)
+addBtn.addEventListener('click',()=> {
+    addList()
+})
+numberInput.addEventListener('keydown',(e)=> {
+    if(e.key === 'Enter'){
+        addList()
     }
-    return randomer
+})
+function addList(){
+    if(textInput.value !== '' && numberInput.value !== ''){
+        const h3 = document.createElement('h3')
+        const span = document.createElement('span')
+        const text = document.createElement('p')
+        text.setAttribute('class','errText')
+        span.innerHTML = 'X'
+        newList.append(h3)
+        newList.append(span)
+        addlist.append(text)
+        h3.innerHTML = `${textInput.value} <br> ${numberInput.value}`
+        textInput.value = ''
+        numberInput.value = ''
+        textInput.style.border = ''
+        numberInput.style.border = ''
+    } else if(textInput.value === '' || numberInput.value === ''){
+        alert('Zapolnite vse stroki')
+        textInput.style.border = '2px solid red'
+        numberInput.style.border = '2px solid red'
+        // textInput.innerText = 'toluk jaz'
+        text.innerHTML = 'toluk jazsan'
+        // textObe.style.display = 'block'
+    }
+
+    span.addEventListener('click',()=> {
+
+    })
 }
-
-
-// myName()
-// function myName(){
-//     window.addEventListener('mousemove', (e)=> {
-//         const target = e.target
-//         if(!target) return
-//         charac.style.left = e.pageX + 'px'
-//         charac.style.top = e.pageY + 'px'
-//     })
-// }
-
-
-
-
-// ls-> kaisy jakta turganyn korsotup beret papka
-// cd -> puti kuda nam nado 
-// cd Desktop -> rab.stolgo otot
-// cd 
-// cd papkanyn aty -> 
-// cd oshol papkanyn aty
